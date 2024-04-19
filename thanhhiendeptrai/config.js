dea20ded06837b0e241998922661cadc4ef86df8
Thanhhien1122 <167502273+Thanhhien1122@users.noreply.github.com>
2024-04-19 12:10:08 +0700

    Add files via upload

---
+++ thanhhiendeptrai/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hê nhô cậu',
+    introDesc: `Tiếp đi có điều bất ngờ nè`,
+    btnIntro: 'hihi',
+    title: 'Nghe nói cậu định bao mềnh chà sữa😙',
+    desc: 'Ehehe mềnh khom có ngại đâu nà :v ',
+    btnYes: 'OKe nha <33',
+    btnNo: 'Hong :3',
+    question:'cho xin cái lịch hẹn ehehe <3',
+    btnReply: 'SEND <3',
+    reply: '7h tối nay nha <33333333',
+    mess: 'Mới test thử tí nè 😘😘',
+    messDesc: 'Thấy cái web xịn hong',
+    btnAccept: 'Quá xịn luônn <3',
+    messLink: 'https://www.facebook.com/hiennguyen3615' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
